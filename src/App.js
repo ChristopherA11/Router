2adb1028eba916963f453eac8d0087276d8d12c7
ChristopherA11 <christopher.a@thydreamtech.com>
2023-12-13 08:34:47 +0530

    router Files added

--- src/App.js
+++ src/App.js
@@ -1,22 +1,37 @@
-import logo from './logo.svg';
-import './App.css';
+import About from './About';
+import Header from './Header';
+import Home from './Home';
+import Missing from './Missing';
+import Nav from './Nav';
+import NewPost from './NewPost';
+import PostPage from './PostPage';
+import Footer from './Footer';
+import { Link, Route, Routes } from 'react-router-dom';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <nav>
+          <ul>
+            <li><Link to="/">Home</Link></li>
+            <li><Link to="/about">About</Link></li>
+            <li><Link to="/newpost">NewPost</Link></li>
+          </ul>
+        </nav>
+        <Routes>
+          <Route path='/' element={<Home/>}/>
+          <Route path='/about' element={<About/>} />
+          <Route path='/newpost'element={<NewPost/>} />
+        </Routes>
+       {/* <Header/> 
+       <Nav />
+       <Home/>
+       <NewPost/>
+       <PostPage/>
+       <About/>
+       <Missing/>
+       <Footer/> */}
       </header>
     </div>
   );
